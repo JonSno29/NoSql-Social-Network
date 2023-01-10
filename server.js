befc4548a2821948d302c64dd7bf542ab40b8513
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2023-01-10 12:24:45 -0700

    updated mongo server

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(require('./routes'))
 
- mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost', {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-
- });
+ mongoose.connect(
+   process.env.MONGODB_URI ||
+     "mongodb://localhost:27017/NOSQL-SOCIAL-NETWORK",
+   {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+   }
+ );
 
  mongoose.set('debug', true)
 
