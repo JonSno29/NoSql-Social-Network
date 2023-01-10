66cef80419b8b0e0e1dabfd60622891d91f7ff8c
JonSno29 <109987633+JonSno29@users.noreply.github.com>
2023-01-09 23:02:53 -0700

    added thought-routes and thought-controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -7,13 +7,13 @@ const thoughtController = {
         path: "reactions", //Thought also populates Thoughts
         select: "-__v", // '-' Don't want it to be returned.
       })
-      .select("-__v") 
+      .select("-__v")
       .then((dbThoughtData) => res.json(dbThoughtData))
       .catch((err) => {
         console.log(err);
         res.status(400).json(err);
       });
-   },
+  },
 
   // get one Thought by id  Get http://localhost:3001/api/Thoughts/<Thought-id-here>
   getThoughtById({ params }, res) {
@@ -31,6 +31,25 @@ const thoughtController = {
       });
   },
 
-
-
-}
+  // createThought Post /api/Thoughts
+  createThought({ body }, res) {
+    Thought.create(body)
+      .then((_id) => {
+        // it associates which id we are creating
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No User found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
+module.exports = thoughtController;
